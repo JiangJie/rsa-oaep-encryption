79bc5c9bcf367ebd6c12436a967847917d08d29e
jarvisjiang <jarvisjiang@tencent.com>
2024-08-18 12:57:01 +0000

    init project structure

---
+++ rollup.config.mjs
@@ -0,0 +1,43 @@
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+
+const input = 'src/mod.ts';
+
+/**
+ * @type {import('rollup').RollupOptions[]}
+ */
+export default [
+    {
+        input,
+        plugins: [
+            esbuild({
+                target: 'esnext',
+            }),
+        ],
+        output: [
+            {
+                file: 'dist/main.cjs',
+                format: 'cjs',
+                sourcemap: true,
+            },
+            {
+                file: 'dist/main.mjs',
+                format: 'esm',
+                sourcemap: true,
+            },
+        ],
+        treeshake: 'smallest',
+    },
+    {
+        input,
+        plugins: [
+            dts(),
+        ],
+        output: {
+            file: 'dist/types.d.ts',
+            format: 'esm',
+            sourcemap: true,
+        },
+        treeshake: 'smallest',
+    },
+];
